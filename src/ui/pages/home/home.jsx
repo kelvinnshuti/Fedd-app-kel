798204e0843dd8f9ebac696d6a01bbd6b8a9bfb5
kelvinnshuti <nshutikelvin0@gmail.com>
2022-08-29 20:58:52 +0200

    image

--- src/ui/pages/home/home.jsx
+++ src/ui/pages/home/home.jsx
@@ -1,8 +1,22 @@
 import './home.css';
+
+import Ill from '../../../assets/illustration.svg';
+
 function Home(){
     return(
-        <div>
-            Home ...
+        <div className='Home'>
+            <div className="home-first">
+                <img className='ill' src={Ill} alt="" />
+            </div>
+            <div className="home-second">
+                
+            </div>
+            <div className="home-third">
+
+            </div>
+            <div className="home-forth">
+
+            </div>
         </div>
     );
 }
