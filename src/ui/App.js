08b2e412aeb0f2e50e570ff4540dd3eb17ebfaeb
kelvinnshuti <nshutikelvin0@gmail.com>
2022-09-20 16:49:05 +0200

    next suit suit

--- src/ui/App.js
+++ src/ui/App.js
@@ -1,7 +1,7 @@
 import Home from './pages/home/home';
 import Career from './pages/career/career';
 import Blog from './pages/blog/blog';
-import { Route, BrowserRouter, Routes } from 'react-router-dom';
+// import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import Signin from './pages/signin/signin';
 // import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
